56a19f078d04746cd3b09a46eb99fa1fe31b2301
Chekkan <harish_babu@outlook.com>
2013-07-01 13:10:41 +0100

    Added the first controller test - HomeController

---
+++ Src/App/Chekkan.Blog.Web/Controllers/HomeController.cs
@@ -0,0 +1,8 @@
+﻿
+using System.Web.Mvc;
+namespace Chekkan.Blog.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+    }
+}
---
+++ Src/Tests/Chekkan.Tests/HomeControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using Chekkan.Blog.Web.Controllers;
+using System.Web.Mvc;
+
+namespace Chekkan.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void SutIsaController()
+        {
+            var sut = new HomeController();
+            Assert.IsAssignableFrom<Controller>(sut);
+        }
+    }
+}
