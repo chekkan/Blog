5686382f0bc8a3fb7f1ba95287c1084929987f1b
Chekkan <harish_babu@outlook.com>
2013-07-02 15:19:32 +0100

    modified files were not added to the last commit

--- Src/App/Chekkan.Blog.Web/Controllers/HomeController.cs
+++ Src/App/Chekkan.Blog.Web/Controllers/HomeController.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System.Web.Mvc;
+
+namespace Chekkan.Blog.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
