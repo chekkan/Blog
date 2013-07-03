b4b89c7a3e0ed34c975ac7994c2db66d5b2fec76
Chekkan <harish_babu@outlook.com>
2013-07-03 10:34:36 +0100

    Added the Login method to AccountController with tests

---
+++ Src/App/Chekkan.Blog.Web/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Chekkan.Blog.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ViewResult Login()
+        {
+            return View();
+        }
+    }
+}
