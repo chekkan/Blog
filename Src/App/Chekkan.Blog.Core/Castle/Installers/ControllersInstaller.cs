af675e88f3a2df62b2c7ef53bf32960627f549d3
Chekkan <harish_babu@outlook.com>
2013-07-04 10:43:41 +0100

    Implementing castle.windsor IoC Container to handle creation of controllers

---
+++ Src/App/Chekkan.Blog.Core/Castle/Installers/ControllersInstaller.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace Chekkan.Blog.Core.Castle.Installers
+{
+    public class ControllersInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Classes.FromAssemblyNamed("Chekkan.Blog.Web"));
+        }
+    }
+}
---
+++ Src/Tests/Chekkan.Blog.Tests/ControllersInstallerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.Mvc;
+using Castle.MicroKernel;
+using Castle.Windsor;
+using Xunit;
+using Chekkan.Blog.Core.Castle.Installers;
+
+namespace Chekkan.Blog.Tests
+{
+    public class ControllersInstallerTests
+    {
+        private IWindsorContainer containerWithControllers;
+
+        public ControllersInstallerTests()
+        {
+            containerWithControllers = new WindsorContainer()
+                .Install(new ControllersInstaller());
+        }
+
+        [Fact]
+        public void All_Controllers_Implement_IController()
+        {
+            var allHandlers = GetAllHandlers(containerWithControllers);
+            var controllerHandlers = GetHandlersFor(typeof(IController), containerWithControllers);
+
+            Assert.NotEmpty(allHandlers);
+            Assert.Equal(allHandlers, controllerHandlers);
+        }
+
+        private IHandler[] GetAllHandlers(IWindsorContainer container)
+        {
+            return GetHandlersFor(typeof(object), container);
+        }
+
+        private IHandler[] GetHandlersFor(Type type, IWindsorContainer container)
+        {
+            return container.Kernel.GetAssignableHandlers(type);
+        }
+    }
+}
