03f80006f7a72ba37a4ced329773352651c89216
Chekkan <harish_babu@outlook.com>
2013-07-04 13:31:45 +0100

    wired up the windsor castle to be used to install all the necessary dependencies

--- Src/App/Chekkan.Blog.Web/Global.asax.cs
+++ Src/App/Chekkan.Blog.Web/Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using Chekkan.Blog.Web.Plumbing;
 
 namespace Chekkan.Blog.Web
 {
@@ -12,6 +15,8 @@ namespace Chekkan.Blog.Web
     // visit http://go.microsoft.com/?LinkId=9394801
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static IWindsorContainer container;
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -19,6 +24,15 @@ namespace Chekkan.Blog.Web
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            MvcApplication.BootStrapContainers();
+        }
+
+        private static void BootStrapContainers()
+        {
+            container = new WindsorContainer()
+                .Install(FromAssembly.This());
+            var controllerFactory = new WindsorControllerFactory(container.Kernel);
+            ControllerBuilder.Current.SetControllerFactory(controllerFactory);
         }
     }
 }
---
+++ Src/Tests/Chekkan.Blog.Tests/WindsorControllerFactoryTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web.Mvc;
+using Castle.MicroKernel;
+using Chekkan.Blog.Web.Plumbing;
+using Moq;
+using Xunit;
+
+namespace Chekkan.Blog.Tests
+{
+    public class WindsorControllerFactoryTests
+    {
+        private Mock<IKernel> mockKernel;
+        private WindsorControllerFactory sut;
+
+        public WindsorControllerFactoryTests()
+        {
+            mockKernel = new Mock<IKernel>();
+            sut = new WindsorControllerFactory(mockKernel.Object);
+
+        }
+
+        [Fact]
+        public void Sut_is_a_IControllerFactory()
+        {
+            Assert.IsAssignableFrom<IControllerFactory>(sut);
+            Assert.IsAssignableFrom<DefaultControllerFactory>(sut);
+        }
+
+        [Fact]
+        public void Sut_throws_ArgumnetNullException_given_null_parameter()
+        {
+            var ex = Assert.Throws<ArgumentNullException>( 
+                () => new WindsorControllerFactory(null));
+            Assert.Equal("kernel", ex.ParamName);
+        }
+
+        [Fact]
+        public void ReleaseController_calls_ReleaseComponent_method_on_kernel()
+        {
+            var stubController = new Mock<IController>();
+
+            sut.ReleaseController(stubController.Object);
+            
+            mockKernel.Verify(k => k.ReleaseComponent(stubController.Object),
+                Times.Once());
+        }
+    }
+}
